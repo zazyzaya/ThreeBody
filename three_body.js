2d0b37c7caf49e73c8929aaa672ce067663e8d75
zazyzaya <zazyzaya@gmail.com>
2023-04-17 22:22:50 -0400

    Added traces

--- three_body.js
+++ three_body.js
@@ -2,10 +2,28 @@ const p1_pic = new Image();
 const p2_pic = new Image();
 const p3_pic = new Image();
 
-const G = 9
-const p_size = 12
-const W = 1000; 
-const H = 1000; 
+const G = 9;
+const p_size = 24;
+const W = document.getElementById('canvas-container').offsetWidth;
+const H = document.getElementById('canvas-container').offsetHeight; 
+
+function generateColor() {
+  let hexSet = "0123456789ABCDEF";
+  let finalHexString = "#";
+  for (let i = 0; i < 6; i++) {
+    finalHexString += hexSet[Math.ceil(Math.random() * 15)];
+  }
+  return finalHexString;
+}
+
+// WHY JAVASCRIPT?!?!?
+function mod(x,div) {
+  var rem = x % div; 
+  if (rem < 0) {
+    return div+rem; 
+  }
+  return rem;
+}
 
 class Planet {
     constructor(x, y, m, vx, vy) {
@@ -14,21 +32,47 @@ class Planet {
         this.m = m;
         this.vx = vx; 
         this.vy = vy; 
-        this.pic = new Image();
 
+        this.pic = new Image();
+        this.color = generateColor();
+ 
         this.bounce_x = 0; 
         this.bounce_y = 0; 
     }
 
+    pixel_trail(ctx) {
+      /*
+      ctx.fillStyle = `rgb(
+        this.rgb[0], this.rgb[1], this.rgb[2]
+      )`;
+      */
+      ctx.beginPath();
+      ctx.fillStyle = this.color; 
+
+      var size = 1+parseInt(this.vnorm(this.vx, this.vy)*5);
+      ctx.arc(mod(this.x, W), mod(this.y, H), size, 0, 2 * Math.PI, false);
+      //ctx.rect(this.x%W, this.y%H, size,size);
+      ctx.fill();
+
+      this.prev_x = this.x; 
+      this.prev_y = this.y; 
+    }
+    
     dist(other) {
-        return Math.sqrt(
-            Math.pow((this.x-other.x), 2) + 
-            Math.pow((this.y-other.y), 2)
-        ); 
+      return Math.sqrt(
+          Math.pow((this.x-other.x), 2) + 
+          Math.pow((this.y-other.y), 2)
+      ); 
     }    
 
     norm() {
-        return Math.sqrt(this.x*this.x + this.y*this.y)
+      return Math.sqrt(this.x*this.x + this.y*this.y)
+    }
+
+    vnorm(x,y) {
+      return Math.sqrt(
+        x*x + y*y
+      )
     }
     
     angle(other) {
@@ -71,8 +115,6 @@ class Planet {
         other.vy, this.vy, 
         other.m, this.m 
       )
-
-      alert({'x': this.bounce_x, 'y':this.bounce_y});
     }
 
     calc_force(other) {
@@ -89,7 +131,7 @@ class Planet {
         // Collision 
         if (d < p_size) {
           this.calc_momentum(other);
-          return; 
+          return;
         }
 
         if (this.x < other.x) {
@@ -130,45 +172,110 @@ class Planet {
       } else {
         this.y = this.y + this.vy; 
       }
+
+      /*
+      this.x = this.x % W; 
+      this.y = this.y % H; 
+
+      if (this.x < 0) {
+        this.x = this.x + W; 
+      }
+      if (this.y < 0) {
+        this.y = this.y + H;
+      }
+      */
     }
 }
 
-p1 = new Planet(300, 150, 1, 0., 0.01);
-p2 = new Planet(200, 200, 1, 0.01, 0.);
-p3 = new Planet(600, 400, 1, -0.1, 0.);
+function rand(high=1, neg=false){
+  if (neg && Math.random() > 0.5){
+    return -Math.random()*high
+  }
+  return Math.random()*high
+}
+
+function rand_pos(axis) {
+  var mid = axis / 2; 
+  var max_dist = mid / 1.05; 
+
+  return mid + rand(max_dist, true); 
+}
+
+p1 = new Planet(rand_pos(W), rand_pos(H), 1+rand(2), rand(0.1, true), rand(0.1, true));
+p2 = new Planet(rand_pos(W), rand_pos(H), 1+rand(2), rand(0.1, true), rand(0.1, true));
+p3 = new Planet(rand_pos(W), rand_pos(H), 1+rand(2), rand(0.1, true), rand(0.1, true));
+
+//var IMG_PIXELS = ctx.getImageData(0,0,W,H);
+
+function set_size(canvas_id) {
+  document.getElementById(canvas_id).setAttribute('width', W);
+  document.getElementById(canvas_id).setAttribute('height', H);
+
+}
+
+function reset() {
+  p1 = new Planet(rand(W), rand(H), 1, rand(0.01, true), rand(0.01, true));
+  p2 = new Planet(rand(W), rand(H), 1, rand(0.01, true), rand(0.01, true));
+  p3 = new Planet(rand(W), rand(H), 1, rand(0.01, true), rand(0.01, true));
+
+  const pctx = document.getElementById("trace").getContext("2d");
+  pctx.clearRect(0,0, W,H);
+}
 
 function init() {
   p1.pic.src = "img/canvas_earth.png";
   p2.pic.src = "img/canvas_earth.png";
   p3.pic.src = "img/canvas_earth.png";
-  window.requestAnimationFrame(draw);
+
+  // Cant do this in css for some reason
+  set_size("planets");
+  set_size("trace");
+  
+  window.requestAnimationFrame(trace);
+  window.requestAnimationFrame(draw_planets);
 }
 
-function draw() {
-  const ctx = document.getElementById("canvas").getContext("2d");
+function trace() {
+  const pctx = document.getElementById("trace").getContext("2d");
+  
+  p1.pixel_trail(pctx);
+  p2.pixel_trail(pctx);
+  p3.pixel_trail(pctx);
 
-  ctx.globalCompositeOperation = "destination-over";
+  window.requestAnimationFrame(trace);
+
+}
+
+function draw_planet(ctx, p) {
+  ctx.translate(mod(p.x, W), mod(p.y, H));
+  ctx.drawImage(p.pic, -12, -12); 
+  ctx.restore();
+  ctx.save();
+}
+
+function draw_planets() {
+  const ctx = document.getElementById("planets").getContext("2d");
+
   ctx.clearRect(0, 0, W, H); // clear canvas
+  //ctx.putImageData(IMG_PIXELS, 0, 0);  // put traces
 
   // Default settings
-  ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
-  ctx.strokeStyle = "rgba(0, 153, 255, 0.4)";
   ctx.save();
 
-  // Earth
-  //const time = new Date();
-  ctx.translate(p1.x%W, p1.y%H);
-  ctx.drawImage(p1.pic, -12, -12);
+  // P1
+  ctx.translate(mod(p1.x, W), mod(p1.y, H));
+  ctx.drawImage(p1.pic, -12, -12); 
   ctx.restore();
   ctx.save();
 
-  // Moon
-  ctx.translate(p2.x%W, p2.y%H);
+  // P2
+  ctx.translate(mod(p2.x, W), mod(p2.y, H));
   ctx.drawImage(p2.pic, -12, -12);
   ctx.restore();
   ctx.save();
 
-  ctx.translate(p3.x%W, p3.y%H);
+  // P3 
+  ctx.translate(mod(p3.x, W), mod(p3.y, H));
   ctx.drawImage(p3.pic, -12, -12);
   ctx.restore();
   ctx.save()
@@ -181,9 +288,7 @@ function draw() {
   p2.update();
   p3.update();
 
-  //calc_force(p1, p3);
-
-  window.requestAnimationFrame(draw);
+  window.requestAnimationFrame(draw_planets);
 }
 
 init();
